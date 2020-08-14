60f411aecae71c7a05122909cc46e1260328a9af
Max Berkowitz <maxl.berkowitz@gmail.com>
2020-08-14 14:40:51 -0400

    added moku test file

---
+++ testMoku.bat
@@ -0,0 +1,25 @@
+@echo off
+goto setup
+:setup
+    start python .\server\app\utils\python\mokuConnection.py 
+    goto channel
+
+:channel
+    set /p answer=Which channel are you testing? (1/2/e)
+    if /i "%answer:~,1%" EQU "1" goto runfirst
+    if /i "%answer:~,1%" EQU "2" goto runsecond
+    if /i "%answer:~,1%" EQU "e" goto done
+    echo Please type 1 for channel 1 or 2 for channel 2 or e to exit
+    goto channel
+
+:runfirst
+    curl -X GET "http://localhost:5000/gen?channel=1&frequency=2&power=3&degrees=4"
+    goto channel
+
+:runsecond
+    curl -X GET "http://localhost:5000/gen?channel=2&frequency=2&power=3&degrees=4"
+    goto channel
+
+:done
+    curl -X GET "http://localhost:5000/shutdown"
+    exit /b
