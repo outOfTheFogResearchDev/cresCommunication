299613c036ee1238cfb525cd609efdc75667d8d6
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-02-08 13:57:20 -0500

    new algorithm complete p1

--- server/app/utils/algorithms/optimizeFrequency old.js
+++ server/app/utils/algorithms/optimizeFrequency old.js
@@ -4,22 +4,35 @@ const { setAnalyzer, resetAnalyzer } = require('../cpp');
 const { asyncLoop } = require('../math');
 const { ms } = require('../time');
 const { storeOptimize, concatCsv } = require('../csv');
-const offPointReset = require('./tools/offPointReset');
-const clearTemp = require('./tools/clearTemp');
 
-module.exports = async (frequency, ampLow, ampHigh, phaseLow, phaseHigh, usingTable) => {
-  if (frequency === usingTable) await offPointReset(frequency);
+module.exports = async (frequency, ampLow, ampHigh, phaseLow, phaseHigh) => {
   await setAnalyzer(frequency);
   await telnet.write(`ac1 1`);
   await telnet.write(`pc1 1`);
+  let previousPowerPoint = [];
   await asyncLoop(ampLow, ampHigh, 20 / 40, async i => {
+    let point = [];
+    let collectPreviousPowerPoint = true;
     await asyncLoop(phaseLow, phaseHigh, 360 / 80, async j => {
-      const point = await optimizePoint(frequency, i, j, usingTable);
+      let newPower = false;
+
+      if (previousPowerPoint.length) {
+        point = previousPowerPoint;
+        previousPowerPoint = [];
+        newPower = true;
+      }
+
+      point = await optimizePoint(frequency, i, j, point, newPower);
+
+      if (collectPreviousPowerPoint) {
+        collectPreviousPowerPoint = false;
+        previousPowerPoint = point;
+      }
+
       await storeOptimize([point], frequency, i, j);
     });
   });
   await ms(1000);
   await concatCsv();
   await resetAnalyzer();
-  if (frequency === usingTable) await clearTemp();
 };
--- server/app/utils/lookupTable/apply.js
+++ server/app/utils/lookupTable/apply.js
@@ -33,7 +33,7 @@ module.exports = async (type, usingTable, prevAmp, prevPhase) => {
   let ps2;
   let pd;
   const powerShift = 0.5; // type === 'fine' ? 0.5 : 1;
-  const degreeShift = 4.5; // type === 'fine' ? 4.5 : 9;
+  const degreeShift = 5; // type === 'fine' ? 5 : 9;
   const otherCornerPower = closest[1] + (amp - closest[3] > 0 ? powerShift : -1 * powerShift);
   const otherCornerDegree = closest[2] + (phase - closest[4] > 0 ? degreeShift : -1 * degreeShift);
   if (
