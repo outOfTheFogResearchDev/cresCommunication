9bc333f6da3a7b9fefa3c2ec1d6d3d110da6d4b6
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-03-04 15:43:24 -0500

    did the thing

--- server/app/utils/lookupTable/applyLarge.js
+++ server/app/utils/lookupTable/applyLarge.js
@@ -1,11 +1,12 @@
 const telnet = require('../telnet');
 const { readLargeTable } = require('../csv');
 
-module.exports = async (ampStep = 1, phaseStep = 10) => {
+module.exports = async (ampStep = 10, phaseStep = 30) => {
   const { frequency, amp, phase } = await telnet.parseGlobalStat();
-  const row =
-    Math.round((amp > 2010 ? 2010 : amp) / ampStep) * (6420 / phaseStep + 1) +
-    Math.round((phase > 6420 ? 6420 : phase) / phaseStep);
+  let _amp = amp > 2010 ? 2010 : amp;
+  _amp = amp < 960 ? 960 : amp;
+  const _phase = phase > 6420 ? 6420 : phase;
+  const row = Math.round((_amp - 960) / ampStep) * (6420 / phaseStep + 1) + Math.round(_phase / phaseStep);
   const { ps1, ps2, pd } = await readLargeTable(frequency, row);
   await telnet.write(`mp 1 1 ${ps1} `);
   await telnet.write(`mp 1 2 ${ps2} `);
