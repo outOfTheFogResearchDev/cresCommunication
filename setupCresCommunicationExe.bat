fcb18eaa6432b0f14babeebf37e702000b064826
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-03-11 11:48:26 -0500

    removed min

--- setupCresCommunicationExe.bat
+++ setupCresCommunicationExe.bat
@@ -6,4 +6,4 @@ del binding.gyp &
 npm i --prod &
 npm run deployment-build &
 echo start chrome /max --app=http://localhost:3330 > ../cresCommunication.bat &
-echo start /min set TYPE=exe^&^&node ./cresCommunication/server/index.js >> ../cresCommunication.bat
+echo set TYPE=exe^&^&node ./cresCommunication/server/index.js >> ../cresCommunication.bat
