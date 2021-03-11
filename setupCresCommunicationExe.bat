75a76dd4c9cd6b10864261bccea6da550a84bfcf
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-03-11 10:33:28 -0500

    take 2

--- setupCresCommunicationExe.bat
+++ setupCresCommunicationExe.bat
@@ -2,6 +2,7 @@ git clone https://github.com/outOfTheFogResearchDev/cresCommunication &
 cd cresCommunication &
 mkdir config &
 echo exports.SECRET = `${Math.random()}`; > config/config.js &
-npm run setup &
+npm i --prod &
+npm run server-build &
 echo start chrome /max --app=http://localhost:3330 > ../cresCommunication.bat &
 echo start /min node set TYPE=local^&^&./cresCommunication/server/index.js >> ../cresCommunication.bat
