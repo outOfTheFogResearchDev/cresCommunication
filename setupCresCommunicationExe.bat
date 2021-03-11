8fcc9ae87b66aa1b5bd06a339b6aca61a98b8441
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-03-10 19:15:09 -0500

    added some ampersands

--- setupCresCommunicationExe.bat
+++ setupCresCommunicationExe.bat
@@ -1,7 +1,7 @@
-git clone https://github.com/outOfTheFogResearchDev/cresCommunication
-cd cresCommunication
-mkdir config
-echo exports.SECRET = `${Math.random()}`; > config/config.js
-npm run setup
-echo start chrome /max --app=http://localhost:3330 > ../cresCommunication.bat
+git clone https://github.com/outOfTheFogResearchDev/cresCommunication &
+cd cresCommunication &
+mkdir config &
+echo exports.SECRET = `${Math.random()}`; > config/config.js &
+npm run setup &
+echo start chrome /max --app=http://localhost:3330 > ../cresCommunication.bat &
 echo start /min node set TYPE=local^&^&./cresCommunication/server/index.js >> ../cresCommunication.bat
