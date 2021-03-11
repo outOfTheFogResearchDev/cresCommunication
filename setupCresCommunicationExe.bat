a5e3a461145207ab37f3b77275bc55cc52d5316c
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-03-11 10:40:35 -0500

    #3

--- setupCresCommunicationExe.bat
+++ setupCresCommunicationExe.bat
@@ -2,6 +2,7 @@ git clone https://github.com/outOfTheFogResearchDev/cresCommunication &
 cd cresCommunication &
 mkdir config &
 echo exports.SECRET = `${Math.random()}`; > config/config.js &
+del binding.gyp
 npm i --prod &
 npm run server-build &
 echo start chrome /max --app=http://localhost:3330 > ../cresCommunication.bat &
