fff1bac6ed05c7516d244e35ec413ebbf7cc6595
Max Berkowitz <maxl.berkowitz@gmail.com>
2021-04-05 14:47:43 -0400

    added offset for MHz edges during table creation

--- server/app/utils/cpp.js
+++ server/app/utils/cpp.js
@@ -1,6 +1,8 @@
-const bindings = require('bindings');
+const bindings = require('bindings'); // eslint-disable-line import/no-extraneous-dependencies
+const { mHZEdgeOffset } = require('./global');
 
 const gp = bindings('getPower');
+const sa = bindings('setAnalyzer');
 
 module.exports = {
   getPower: async () => {
@@ -15,6 +17,12 @@ module.exports = {
     return Math.round(power * 10) / 10;
   },
   resetAnalyzer: bindings('resetAnalyzer'),
-  setAnalyzer: bindings('setAnalyzer'),
+  setAnalyzer: async (freq, creatingTable) => {
+    let frequency = freq;
+    if (creatingTable) {
+      frequency = mHZEdgeOffset(frequency);
+    }
+    return sa(frequency);
+  },
   setCenter: bindings('setCenter'),
 };
--- server/app/utils/global.js
+++ server/app/utils/global.js
@@ -6,9 +6,18 @@ module.exports = {
   },
   getFrequency: () => frequency,
   getTableFrequency: () => {
-    let temp = Math.floor((frequency - 102.5) / 5);
+    let temp = Math.floor((frequency - 97.5) / 5);
     if (temp < 0) temp = 0;
     if (temp > 20) temp = 20;
     return temp * 5 + 100;
+  },
+  mHZEdgeOffset: freq => {
+    if (freq === 100) {
+      // return 101.25;
+    }
+    if (freq === 200) {
+      // return 198.75;
+    }
+    return freq;
   },
 };
