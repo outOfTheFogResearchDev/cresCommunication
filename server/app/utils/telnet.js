518d82b2555dcf0f3dd524a2df1bd0fdfa6cdbb7
Max-Berkowitz <maxl.berkowitz@gmail.com>
2021-06-08 17:36:14 -0400

    added fixes for new firmware update

--- server/app/utils/telnet.js
+++ server/app/utils/telnet.js
@@ -15,44 +15,44 @@ module.exports = {
   async setFreq(frequency) {
     setFrequency(frequency);
     let code = Math.round(frequency * 10);
-    if (code < 100) code = 100;
-    if (code > 200) code = 200;
-    await this.write(`sf ${(code / 10).toFixed(1)}`);
+    if (code < 1000) code = 1000;
+    if (code > 2000) code = 2000;
+    await this.write(`sf ${(code / 10).toFixed(1)} `);
   },
   async getAmpPhaseCodes() {
-    const { amplitude: amp, phase_360: phase } = JSON.parse(await this.write('apd'));
+    const { amplitude: amp, phase_360: phase } = JSON.parse(await this.write('apd '));
     return { amp, phase };
   },
   async parseGlobalStat() {
-    const globalStat = await this.write('GlobalStat');
-    let index = globalStat.indexOf('Frequency:');
-    const frequency = +globalStat.substring(index + 11, index + 14);
-    index = globalStat.indexOf('Phase 1:', index + 14);
-    const phase1 = +globalStat.substring(index + 12, index + 16);
-    index = globalStat.indexOf('Phase 2:', index + 16);
-    const phase2 = +globalStat.substring(index + 12, index + 16);
-    index = globalStat.indexOf('Amplitude:', index + 16);
-    const amp = +globalStat.substring(index + 12, index + 16);
-    index = globalStat.indexOf('360 Phase:', index + 16);
-    let phase = +globalStat.substring(index + 12, index + 16);
-    if (phase === 0) {
-      if (phase1 > 1000 && phase2 > 1000) {
-        phase = 3230;
-      }
-      if (phase1 < 1000 && phase2 > 1000) {
-        phase = 1615;
-      }
-      if (phase1 > 1000 && phase2 < 1000) {
-        phase = 4850;
-      }
-    }
-    index = globalStat.indexOf('PS1', index + 16);
-    const ps1 = +globalStat.substring(index + 15, index + 20);
-    index = globalStat.indexOf('PS2', index + 20);
-    const ps2 = +globalStat.substring(index + 15, index + 20);
-    index = globalStat.indexOf('PD', index + 20);
-    const pd = +globalStat.substring(index + 14, index + 19);
-    return { frequency, phase1, phase2, amp, phase, ps1, ps2, pd };
+    // const globalStat = await this.write('GlobalStat ');
+    // let index = globalStat.indexOf('Frequency:');
+    // const frequency = +globalStat.substring(index + 11, index + 14);
+    // index = globalStat.indexOf('Phase 1:', index + 14);
+    // const phase1 = +globalStat.substring(index + 12, index + 16);
+    // index = globalStat.indexOf('Phase 2:', index + 16);
+    // const phase2 = +globalStat.substring(index + 12, index + 16);
+    // index = globalStat.indexOf('Amplitude:', index + 16);
+    // const amp = +globalStat.substring(index + 12, index + 16);
+    // index = globalStat.indexOf('360 Phase:', index + 16);
+    // let phase = +globalStat.substring(index + 12, index + 16);
+    // if (phase === 0) {
+    //   if (phase1 > 1000 && phase2 > 1000) {
+    //     phase = 3230;
+    //   }
+    //   if (phase1 < 1000 && phase2 > 1000) {
+    //     phase = 1615;
+    //   }
+    //   if (phase1 > 1000 && phase2 < 1000) {
+    //     phase = 4850;
+    //   }
+    // }
+    // index = globalStat.indexOf('PS1', index + 16);
+    // const ps1 = +globalStat.substring(index + 15, index + 20);
+    // index = globalStat.indexOf('PS2', index + 20);
+    // const ps2 = +globalStat.substring(index + 15, index + 20);
+    // index = globalStat.indexOf('PD', index + 20);
+    // const pd = +globalStat.substring(index + 14, index + 19);
+    // return { frequency, phase1, phase2, amp, phase, ps1, ps2, pd };
   },
   connect() {
     return new Promise(resolve => {
