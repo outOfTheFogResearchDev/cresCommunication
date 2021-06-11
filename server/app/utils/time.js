d0f6fc160f2127a53eee684ec4ffa2642e5060b8
Max-Berkowitz <maxl.berkowitz@gmail.com>
2021-06-11 17:01:48 -0400

    added timers for optimizePoint

--- server/app/utils/time.js
+++ server/app/utils/time.js
@@ -1,3 +1,30 @@
+let previousMs = 0;
+const data = {
+  code: { totalTime: 0, quantity: 0, averageTime: 0 },
+  telnet: { totalTime: 0, quantity: 0, averageTime: 0 },
+  moku: { totalTime: 0, quantity: 0, averageTime: 0 },
+  analyzer: { totalTime: 0, quantity: 0, averageTime: 0 },
+};
+
 module.exports = {
   ms: ms => new Promise(resolve => setTimeout(resolve, ms)),
+  clock: type => {
+    data[type].totalTime += Date.now() - previousMs;
+    data[type].quantity += 1;
+    previousMs = Date.now();
+  },
+  clockStart: () => {
+    previousMs = Date.now();
+  },
+  clockPrint: () => {
+    Object.entries(data).forEach(([type, values]) => {
+      data[type].averageTime = values.totalTime / values.quantity;
+    });
+    console.log(data); // eslint-disable-line no-console
+    Object.entries(data).forEach(([type, values]) => {
+      Object.entries(values).forEach(([value]) => {
+        data[type][value] = 0;
+      });
+    });
+  },
 };
